9424f2319f562afb05364ab1cf6ef39e4adf3dea
Milad Joodi <winbeta.com@gmail.com>
2024-08-26 01:33:51 +0330

    up

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,9 +1,23 @@
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
-    return (
-        <div>
-            Admin Page
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* Left */}
+      <div className="w-full lg:w-2/3">
+        {/* User Cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
         </div>
-    );
-}
+      </div>
+
+      {/* Right */}
+      <div className="w-full lg:w-1/3">Right</div>
+    </div>
+  );
+};
 
 export default AdminPage;
