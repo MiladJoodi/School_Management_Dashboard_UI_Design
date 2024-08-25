feba22981337a479dc6e87b264f6a862711111db
Milad Joodi <winbeta.com@gmail.com>
2024-08-26 00:17:07 +0330

    up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <div className="flex items-center justify-between p-4">
             {/* Seach bar */}
-            <div className="hidden md:flex items-center gap-2 text-xs roundd-full ring-[1.5px] ring-gray-300 px-2">
+            <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
                 <Image src="/search.png" alt="" width={14} height={14} />
                 <input type="text" placeholder="Search..." className="w-[200px] p-2 bg-transparent outline-none"  />
             </div>
