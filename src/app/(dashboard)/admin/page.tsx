f2a2189fd5826b09cea363227a69bf1aeb4be1a3
Milad Joodi <winbeta.com@gmail.com>
2024-08-28 20:32:17 +0330

    Events Finished

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import Announcements from "@/components/Announcements";
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
 import EventCalendar from "@/components/EventCalendar";
@@ -39,6 +40,7 @@ const AdminPage = () => {
       {/* Right */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
         <EventCalendar />
+        <Announcements />
       </div>
     </div>
   );
