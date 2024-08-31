27052f3420871e108fcd904a48e9e925bb867b12
Milad Joodi <winbeta.com@gmail.com>
2024-08-31 22:35:39 +0330

    0

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,17 +1,26 @@
-
-
-const Table = ({columns} : {columns:{header: string, accessor: string, className?: string}[]}) => {
-    return (
-        <table className="w-full mt-4">
-            <thead>
-                <tr className="text-left text-gray-500 text-sm">
-                    {columns.map((col)=>(
-                        <th key={col.accessor}>{col.header}</th>
-                    ))}
-                </tr>
-            </thead>
-        </table>
-    );
-}
+const Table = ({
+  columns,
+  renderRow,
+  data
+}: {
+  columns: { header: string; accessor: string; className?: string }[];
+  renderRow: (item: any) => React.ReactNode;
+  data: any[]
+}) => {
+  return (
+    <table className="w-full mt-4">
+      <thead>
+        <tr className="text-left text-gray-500 text-sm">
+          {columns.map((col) => (
+            <th key={col.accessor} className={col.className}>{col.header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item)=> renderRow(item))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Table;
